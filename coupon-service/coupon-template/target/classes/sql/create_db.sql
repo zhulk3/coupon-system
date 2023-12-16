502074ed5cf2a257dd524c68e98d263e40c570f1
zhulk3 <zhulk3@outlook.com>
2023-12-16 09:55:49 +0800

    update .gitignore

--- coupon-service/coupon-template/target/classes/sql/create_db.sql
+++
@@ -1,8 +0,0 @@
--- 登录 MySQL 服务器
-mysql -hlocalhost -uzhulk3 -pa12345
-
--- 创建数据库 imooc_coupon_data
-CREATE DATABASE IF NOT EXISTS coupon_data;
-
--- 登录 MySQL 服务器, 并进入到 coupon_data 数据库中
-mysql -hlocalhost -uroot -pcaiculati2on. -Dcoupon_data
